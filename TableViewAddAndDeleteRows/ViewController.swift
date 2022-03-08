5752b4467844ebe09f56a27a74a69a4f03cd89a9
Michael Miles <michael.miles@seasoned.co>
2022-03-08 16:35:47 -0500

    Set up initial data for project

--- TableViewAddAndDeleteRows/ViewController.swift
+++ TableViewAddAndDeleteRows/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    private let CELL_ID = "CELL_ID"
+    
+    private var fastFoods = [
+        "McDonalds",
+        "Wendys",
+        "Burger King"
+    ]
+    
+    private var newFastFoods = [
+        "Chick-fil-A",
+        "Chipotle"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.register(UITableView.self, forCellReuseIdentifier: CELL_ID)
     }
+}
 
-
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fastFoods.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath)
+        cell.textLabel?.text = fastFoods[indexPath.row]
+        return cell
+    }
 }
 
